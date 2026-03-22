83de6b07dfba04f78072bf0ac0737572fc20fc23
shivm2003 <2003shivam1990@gmail.com>
2026-03-22 06:48:59 +0530

    Logo

--- my-frontend/src/components/common/Header/Header.jsx
+++ my-frontend/src/components/common/Header/Header.jsx
@@ -5,7 +5,7 @@ import { APP_NAME } from '../../../utils/constants';
 import { useAuth } from '../../../contexts/AuthContext';
 import { useCart } from '../../../contexts/CartContext'; // ✅ Added cart context
 import './index.css';
-import logo from "../../../../assets/logo.png";
+import logo from "../../../../assets/Logo.png";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
